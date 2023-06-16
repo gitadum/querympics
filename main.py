725d2f9ca95cbdd1f3fb343f26c3f9d4a4049850
Adrien Dumont <dev@adum.io>
2023-06-16 20:10:17 +0200

    fichier d'interaction avec la base PostGre

---
+++ main.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import psycopg2
+
+# Connexion à la base de données PostGre
+connector = psycopg2.connect(host="localhost",
+                             dbname="olympics",
+                             user="sqladum",
+                             password="dummypassw0rd",
+                             port=5432)
+cursor = connector.cursor()
+
+# Ici on lancera les requêtes qui iront bien
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS athlete (
+
+id VARCHAR(7) PRIMARY KEY,
+name VARCHAR(128),
+sex CHAR,
+age INT,
+team VARCHAR(64),
+noc VARCHAR(3),
+games VARCHAR(11),
+year INT,
+season VARCHAR(6),
+city VARCHAR(32),
+event VARCHAR(128),
+medal VARCHAR(6)
+
+);
+""")
+
+connector.commit()
+cursor.close()
+connector.close()
+
+print("Query ended.")
