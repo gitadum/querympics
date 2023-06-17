d8b86dd626923d2d2fe293807580561d48d4b59a
Adrien Dumont <dev@adum.io>
2023-06-17 13:22:43 +0200

    remplissage de la table region (via sqlalchemy)

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import pandas as pd
 import psycopg2
+from sqlalchemy import create_engine
+
+db_usr = "sqladum"
+db_pwd = "dummypassw0rd"
+db_name = "olympics"
+db_host = "localhost"
+db_port = 5432
+
+engine_path = f"postgresql+psycopg2://{db_usr}:{db_pwd}@{db_host}/{db_name}"
+engine = create_engine(engine_path)
+
+data_dir = "./data/"
+os.chdir(data_dir)
+
+region = pd.read_csv("regions.csv")
 
 # Connexion à la base de données PostGre
-connector = psycopg2.connect(host="localhost",
-                             dbname="olympics",
-                             user="sqladum",
-                             password="dummypassw0rd",
-                             port=5432)
+connector = psycopg2.connect(host=db_host, dbname=db_name,
+                             user=db_usr, password=db_pwd,
+                             port=db_port)
 cursor = connector.cursor()
 
 # Ici on lancera les requêtes qui iront bien
@@ -40,6 +55,9 @@ cursor.execute(
     );
     """
 )
+
+# Remplissage de la table region
+region.to_sql("region", engine, if_exists="append", index=False)
 
 connector.commit()
 cursor.close()
