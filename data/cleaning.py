3419e13e9ce2c6f01773ffb64babaffb7f8c4f33
Adrien Dumont <dev@adum.io>
2023-06-19 17:50:49 +0200

    bascule des fichiers de données dans data/files

--- data/cleaning.py
+++ data/cleaning.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 # Lecture des données
-os.chdir("./data/")
+os.chdir("./data/files/")
 summer = pd.read_csv("Athletes_summer_games.csv")
 winter = pd.read_csv("Athletes_winter_games.csv")
 region = pd.read_csv("regions.csv", index_col=0)
