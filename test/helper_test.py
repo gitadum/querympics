1d8fccd024e2bc9a801f50d68761cb8e41145aad
Adrien Dumont <dev@adum.io>
2023-06-19 17:16:27 +0200

    les tests ne sont plus masqués par gitignore

---
+++ test/helper_test.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import utils.helper
+import pytest
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ("Michael Phelps", ("Michael", "PHELPS")),
+        ("Zsuzsanna 'Zsu' Jakabos", ("Zsuzsanna", "JAKABOS")),
+        ("JAKABOS Zsuzsanna", ("Zsuzsanna", "JAKABOS")),
+    )
+)
+def test_parse_name(input_name, expected):
+    assert utils.helper.parse_name(input_name) == expected
