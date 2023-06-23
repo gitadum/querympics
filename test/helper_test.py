5c89997c3dbc5abf71c05414fbc5a557d4a16646
Adrien Dumont <dev@adum.io>
2023-06-23 14:28:14 +0200

    give_person_id est testé sur la dernière version

--- test/helper_test.py
+++ test/helper_test.py
@@ -25,13 +25,15 @@ def test_give_short_name(input_name, expected):
     assert utils.helper.give_short_name(input_name) == expected
 
 @pytest.mark.parametrize(
-        ("input_first_name", "input_last_name", "input_gender", "expected"),
+        ("input_first_name", "input_last_name", "input_gender", "input_yob",
+         "expected"),
     (
-        ("Alex", "MORGAN", "F", "ALEXMORG0"),
+        ("Alex", "MORGAN", "F", 1989, "ALEXMORGAN01989"),
     )
 )
 def test_give_person_id(input_first_name, input_last_name, input_gender,
-                        expected):
+                        input_yob, expected):
     assert utils.helper.give_person_id(input_first_name,
                                        input_last_name,
-                                       input_gender) == expected
+                                       input_gender,
+                                       input_yob) == expected
