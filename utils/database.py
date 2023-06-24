a158b0ae7a3329bcf754c5497c658ec18af17ba9
Adrien Dumont <dev@adum.io>
2023-06-24 15:32:13 +0200

    create prend en argument un fichier de requête

--- utils/database.py
+++ utils/database.py
@@ -6,11 +6,14 @@ import psycopg2
 import sqlalchemy
 
 def create(db_name,
-           query,
+           query_file_path,
            connector: psycopg2.connection,
            cursor: psycopg2.cursor,
            verbose = True):
     print(f"Crating `{db_name}`...") if verbose else None
+    with open(query_file_path, "r") as f:
+        query = f.read()
+    query = query.split(";")[0]
     cursor.execute(query)
     connector.commit()
     print(f"Created `{db_name}`.") if verbose else None
