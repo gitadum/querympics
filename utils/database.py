b2c84fa2739af173695d991b5201fc3055f88e2d
Adrien Dumont <dev@adum.io>
2023-06-24 15:57:11 +0200

    database: application de différents correctifs

--- utils/database.py
+++ utils/database.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-import psycopg2
-import sqlalchemy
 
 def create(db_name,
            query_file_path,
-           connector: psycopg2.connection,
-           cursor: psycopg2.cursor,
+           connector,
+           cursor,
            verbose = True):
     print(f"Crating `{db_name}`...") if verbose else None
     with open(query_file_path, "r") as f:
@@ -20,8 +18,8 @@ def create(db_name,
 
 def truncate(db_name,
              query_file_path,
-             connector: psycopg2.connection,
-             cursor: psycopg2.cursor,
+             connector,
+             cursor,
              verbose = True):
     print(f"Truncating `{db_name}`...") if verbose else None
     with open(query_file_path, "r") as f:
@@ -29,12 +27,12 @@ def truncate(db_name,
     query = query.split(";")[0]
     cursor.execute(query)
     connector.commit()
-    print("Truncated `{db_name}`.") if verbose else None
+    print(f"Truncated `{db_name}`.") if verbose else None
 
 def fill_table_from_csv(db_name,
                         file_path,
-                        connector: psycopg2.connection,
-                        engine: sqlalchemy.Engine,
+                        connector,
+                        engine,
                         verbose = True,
                         fill_by_chuncks = False,
                         n_chunk = 10000):
@@ -46,11 +44,11 @@ def fill_table_from_csv(db_name,
         i = 0
         for chunk in pd.read_csv(file_path, chunksize=n_chunk):
             chunk.to_sql(db_name, engine, if_exists="append", index=False)
-            i += n_chunk
+            i += chunk.shape[0]
             print(f"loaded {i} records into {db_name}.") if verbose else None
     else:
         # Remplissage de la table en une seule fois
         df = pd.read_csv(file_path)
         df.to_sql(db_name, engine, if_exists="append", index=False)
     connector.commit()
-    print("Done with loading records into `athlete`.") if verbose else None
+    print(f"Done with loading records into `{db_name}`.") if verbose else None
