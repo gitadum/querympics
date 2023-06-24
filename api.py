ce8833696c7b248f9c5d9307d128b642d6a7612f
Adrien Dumont <dev@adum.io>
2023-06-24 17:19:07 +0200

    ajout de fonctions asynchrones pour les composants

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
 
 import uvicorn
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,6 +11,14 @@ app = FastAPI()
 @app.get("/")
 async def greetings():
     return {"greetings": "Welcome to Querylimpics!"}
+
+@app.get("/component/{component_id}") # path parameter
+async def get_component(component_id: int):
+    return {"component": component_id}
+
+@app.get("/component/")
+async def read_component(number: int, text: Optional[str]):
+    return {"number": number, "text": text}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
