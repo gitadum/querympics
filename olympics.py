d2a0b0a442b698d1c53ac47828bd8f4af54b9ed6
Adrien Dumont <dev@adum.io>
2023-06-25 10:57:03 +0200

    suppression des deux exemples de résultat

--- olympics.py
+++ olympics.py
@@ -31,25 +31,3 @@ class Athlete(BaseModel):
     gender: str
     birth_year: Optional[str]
     lattest_noc: Optional[str]
-
-
-result1 = ResultIn(
-    id="S000001",
-    games="S1992",
-    sport="Basketball",
-    event="Basketball Men's Basketball",
-    athlete="995417935682",
-    noc="CHN",
-    password=password
-)
-
-result2 = ResultIn(
-    id="S000004",
-    games="S1900",
-    sport="Tug-Of-War",
-    event="Tug-Of-War Men's Tug-Of-War",
-    athlete="341882753977",
-    noc="DEN",
-    medal="G",
-    password=password
-)
