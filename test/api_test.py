9f65fc21a9e39056e7f0f4b77ac7eb8332a3f5bd
Adrien Dumont <dev@adum.io>
2023-06-25 19:33:43 +0200

    script de tests sur les requêtes de l'API

---
+++ test/api_test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from fastapi.testclient import TestClient
+from api import app
+
+client = TestClient(app)
+
+def test_greetings():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"greetings": "Welcome to QUERYMPICS!"}
