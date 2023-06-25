8051ebc5513e820fae044cd1846e0e3404d6dcb8
Adrien Dumont <dev@adum.io>
2023-06-25 15:18:32 +0200

    ajout d'une classe Athlete

--- models.py
+++ models.py
@@ -24,3 +24,17 @@ class Result(models.Model):
 Result_Pydantic = pydantic_model_creator(Result, name="Result")
 ResultIn_Pydantic = pydantic_model_creator(Result, name="ResultIn",
                                            exclude_readonly=True)
+
+
+class Athlete(models.Model):
+
+    id = fields.CharField(pk=True, max_length=12)
+    first_name = fields.CharField(max_length=128, null=True)
+    last_name = fields.CharField(max_length=128, null=True)
+    gender = fields.CharField(max_length=1, null=True)
+    birth_year = fields.SmallIntField(null=True)
+    lattest_noc = fields.CharField(max_length=3, null=True)
+
+Athlete_Pydantic = pydantic_model_creator(Athlete, name="Athlete")
+AthleteIn_Pydantic = pydantic_model_creator(Athlete, name="AthleteIn",
+                                            exclude_readonly=True)
