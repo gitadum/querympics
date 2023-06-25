e0ccffbcb8aa5b9687faf3b4e8634639ecb5193c
Adrien Dumont <dev@adum.io>
2023-06-25 20:32:09 +0200

    switch to v0.1.1-alpha

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 
 from setuptools import setup, find_packages
 
-setup(name='querympics', version="0.1.0", packages=find_packages())
+setup(name='querympics', version="0.1.1-alpha", packages=find_packages())
