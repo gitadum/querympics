eaaba08a6b22dd436f30db840169c5218413a79b
Adrien Dumont <dev@adum.io>
2023-06-26 12:03:45 +0200

    instanciation de setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name='querympics', version="0.1.1-beta", packages=find_packages())
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='querympics',
+    description="Une API pour trouver des infos sur les jeux olympiques",
+    package_dir={"": "app"},
+    version="0.1.1-beta",
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    link="https://github.com/gitadum/querympics",
+    author="adum",
+    author_email="dev@adum.io",
+    license="GPL-3.0",
+    python_requires="==3.8.*")
