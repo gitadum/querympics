024a46d1ca6714125b3847fdfa45b0cb4eae97c5
Adrien Dumont <dev@adum.io>
2023-06-29 10:43:35 +0200

    ajout de l'id athlète dans la vue athlète

--- app/data/base/athlete_view.create.sql
+++ app/data/base/athlete_view.create.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW athlete_view AS (
-    SELECT first_name, last_name, gender, birth_year,
+    SELECT athlete.id, first_name, last_name, gender, birth_year,
     ARRAY_AGG(DISTINCT result.noc) AS nocs,
     ARRAY_AGG(DISTINCT result.sport) AS disciplines,
     COUNT(medal) AS n_medals
