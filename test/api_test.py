7afc50e30aac953386cd02ae53480d70455423bd
Adrien Dumont <dev@adum.io>
2023-07-08 16:10:09 +0200

    api_test: test de post result

--- test/api_test.py
+++ test/api_test.py
@@ -30,3 +30,34 @@ def test_get_a_result(input_id, expected):
         response = client.get(f"/result/{input_id}")
         assert response.status_code == 200
         assert response.json() == expected
+
+
+@pytest.mark.parametrize(
+        ("input_post", "expected"),
+        (
+            ({
+                "season": "Winter",
+                "year": "2014",
+                "sport": "Ice Swimming",
+                "event": "Ice Swimming Women's 200m Butterfly",
+                "athlete": "410421217148",
+                "noc": "HUN",
+                "medal": "S"
+              },
+             {
+                "id": "W048565",
+                "games": "W2014",
+                "sport": "Ice Swimming",
+                "event": "Ice Swimming Women's 200m Butterfly",
+                "athlete": "410421217148",
+                "noc": "HUN",
+                "medal": "S"
+              }),
+        )
+)
+def test_write_a_result(input_post, expected):
+    with TestClient(app) as client:
+        response = client.post("/result/new", params=input_post)
+        assert response.status_code == 200
+        assert response.json() == expected
+        client.delete(f"/result/{response.json()['id']}")
