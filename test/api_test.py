777e1db7ea413e80d2705ed6090e1e8838116006
Adrien Dumont <dev@adum.io>
2023-07-08 15:30:14 +0200

    correction bug import test client - erreur "DatabaseBackend Not Running"

--- test/api_test.py
+++ test/api_test.py
@@ -3,10 +3,30 @@
 
 from fastapi.testclient import TestClient
 from app.api import app
-
-client = TestClient(app)
+import pytest
 
 def test_greetings():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"greetings": "Welcome to QUERYMPICS!"}
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"greetings": "Welcome to QUERYMPICS!"}
+
+@pytest.mark.parametrize(
+    ("input_id", "expected"),
+    (
+        ("S121453", {
+                        "id": "S121453",
+                        "games": "S2004",
+                        "sport": "Swimming",
+                        "event": "Swimming Women's 400 metres Freestyle",
+                        "athlete": "506160875731",
+                        "noc": "FRA",
+                        "medal": "G"
+                    }),
+    )
+)
+def test_get_a_result(input_id, expected):
+    with TestClient(app) as client:
+        response = client.get(f"/result/{input_id}")
+        assert response.status_code == 200
+        assert response.json() == expected
