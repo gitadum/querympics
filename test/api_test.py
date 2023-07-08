87a45a6927f1974add33c0647574af654e34cbea
Adrien Dumont <dev@adum.io>
2023-07-08 17:07:42 +0200

    api_test: test de put result

--- test/api_test.py
+++ test/api_test.py
@@ -5,6 +5,50 @@ from fastapi.testclient import TestClient
 from app.api import app
 import pytest
 
+# EXEMPLES #
+
+# Result
+lmanaudou2004_swim = {
+                        "id": "S121453",
+                        "games": "S2004",
+                        "sport": "Swimming",
+                        "event": "Swimming Women's 400 metres Freestyle",
+                        "athlete": "506160875731",
+                        "noc": "FRA",
+                        "medal": "G"
+                      }
+
+lmanaudou2004_boxing = {
+                        "id": "S121453",
+                        "games": "S2004",
+                        "sport": "Boxing",
+                        "event": "Swimming Women's 400 metres Freestyle",
+                        "athlete": "506160875731",
+                        "noc": "FRA",
+                        "medal": "G"
+                      }
+
+zjakabos_ice_swimming_input = {
+                                "season": "Winter",
+                                "year": "2014",
+                                "sport": "Ice Swimming",
+                                "event": "Ice Swimming Women's 200m Butterfly",
+                                "athlete": "410421217148",
+                                "noc": "HUN",
+                                "medal": "S"
+                                }
+
+zjakabos_ice_swimming_output = {
+                                "id": "W048565",
+                                "games": "W2014",
+                                "sport": "Ice Swimming",
+                                "event": "Ice Swimming Women's 200m Butterfly",
+                                "athlete": "410421217148",
+                                "noc": "HUN",
+                                "medal": "S"
+                                }
+
+
 def test_greetings():
     with TestClient(app) as client:
         response = client.get("/")
@@ -14,15 +58,7 @@ def test_greetings():
 @pytest.mark.parametrize(
     ("input_id", "expected"),
     (
-        ("S121453", {
-                        "id": "S121453",
-                        "games": "S2004",
-                        "sport": "Swimming",
-                        "event": "Swimming Women's 400 metres Freestyle",
-                        "athlete": "506160875731",
-                        "noc": "FRA",
-                        "medal": "G"
-                    }),
+        (lmanaudou2004_swim["id"], lmanaudou2004_swim),
     )
 )
 def test_get_a_result(input_id, expected):
@@ -35,24 +71,7 @@ def test_get_a_result(input_id, expected):
 @pytest.mark.parametrize(
         ("input_post", "expected"),
         (
-            ({
-                "season": "Winter",
-                "year": "2014",
-                "sport": "Ice Swimming",
-                "event": "Ice Swimming Women's 200m Butterfly",
-                "athlete": "410421217148",
-                "noc": "HUN",
-                "medal": "S"
-              },
-             {
-                "id": "W048565",
-                "games": "W2014",
-                "sport": "Ice Swimming",
-                "event": "Ice Swimming Women's 200m Butterfly",
-                "athlete": "410421217148",
-                "noc": "HUN",
-                "medal": "S"
-              }),
+            (zjakabos_ice_swimming_input, zjakabos_ice_swimming_output),
         )
 )
 def test_write_a_result(input_post, expected):
@@ -60,4 +79,22 @@ def test_write_a_result(input_post, expected):
         response = client.post("/result/new", params=input_post)
         assert response.status_code == 200
         assert response.json() == expected
+        # On supprime l'entrée créée pour le test
         client.delete(f"/result/{response.json()['id']}")
+
+@pytest.mark.parametrize(
+        ("input_id", "input_update", "expected"),
+        (
+            (lmanaudou2004_swim["id"],
+             {"sport": "Boxing"},
+             lmanaudou2004_boxing),
+        )
+)
+def test_update_a_result(input_id, input_update, expected):
+    with TestClient(app) as client:
+        original_input = client.get(f"/result/{input_id}").json()
+        response = client.put(f"/result/{input_id}", params=input_update)
+        assert response.status_code == 200
+        assert response.json() == expected
+        # On rétablit les valeurs modifiées pour le test à leur état d'origine
+        client.put(f"/result/{input_id}", params=original_input)
